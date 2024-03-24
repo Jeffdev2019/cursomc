68ad0a5d807573a03d28d4c1f55f731414c2cc16
Jefferson victor da silva <jeffersonvic1@gmail.com>
2024-03-24 03:08:59 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/victor/cursomc/CursomcApplication.java
+++ src/main/java/com/victor/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.victor.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.victor.cursomc.domain.Categoria;
+import com.victor.cursomc.domain.Produto;
 import com.victor.cursomc.repository.CategoriaRepository;
+import com.victor.cursomc.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -24,7 +28,22 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		categoriaRepository.saveAll(Arrays.asList(new Categoria(null, "Informática"), new Categoria(null, "Escritorio")));
+		var cat1 = new Categoria(null, "Informática");
+		var cat2 = new Categoria(null, "Escritorio");
+		
+		var p1 = new Produto(null, "Computador", 2000.00);
+		var p2 = new Produto(null, "Impressora", 800.00);
+		var p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		
 	}
